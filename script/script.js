a1b46c0cc573a5a781fc2d2f6ccf3ca81850998e
issei416 <mukeshkj2912@gmail.com>
2024-06-26 22:34:34 +0530

    wordgram

---
+++ script/script.js
@@ -0,0 +1,9 @@
+let header = document.getElementsByClassName("header")
+let mian = document.getElementsByClassName("main")
+
+let title = document.createElement("h1")
+title.innerText = "WORDGRAM"
+title.classList.add("header","text-center","display-2")
+let tagline = document.createElement("p")
+tagline.classList.add("text-center", "lead");
+
